62446f169dbff548d2550b940ff8deefa42b1b87
h3nrique <henrique.o@ipvc.pt>
2023-06-13 12:54:22 +0100

    first commit

---
+++ src/static/js/scroll.js
@@ -0,0 +1,9 @@
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+    window.onscroll=function(){};
+}
