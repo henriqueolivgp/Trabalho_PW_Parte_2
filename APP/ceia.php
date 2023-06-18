86eb18d87d698fda6f162d5bd1b8caf16c94dbfd
henriqueolivgp <henrique.o@ipvc.pt>
2023-06-18 20:42:54 +0100

    Some changes in flora class's

--- APP/ceia.php
+++ APP/ceia.php
@@ -19,31 +19,75 @@ require_once __DIR__ . '/Compunents/header.php';
 
   <!--Navbar finished here-->
 
-  <div class="container  h-100">
-    <h1 class="page-title mt-4  text-center ">Ceia</h1>
-    <!-- conteúdo da página aqui -->
-    <div class="container2 " ">
-      <div class=" row justify-content-center" style="margin-top:4%;">
-      <div class="col-md-4 mb-5 p-5" style="background-color: #81a05a; height: 400px; width: 75%; border: 8px solid #198754; border-radius: 1rem;">
-        <h2>Ola</h2>
-      </div>
-      <div class="col-md-4 mb-5 p-5" style="background-color: #81a05a; height: 400px; width: 75%; border: 8px solid #198754; border-radius: 1rem;">
-        <h2>Ola</h2>
-      </div>
-      <div class="col-md-4 mb-5 p-5" style="background-color: #81a05a; height: 400px; width: 75%; border: 8px solid #198754; border-radius: 1rem;">
-        <h2>Ola</h2>
+  <body>
+
+    <div class="container">
+      <h1 class="page-title mt-4  text-center ">Ceia</h1>
+      <!-- conteúdo da página aqui -->
+      <div class="card mb-3 align-items-center ">
+        <div class="row d-flex align-items-center w-100">
+          <div class="col-xl-4 col-lg-5 col  p-4">
+            <img src="../recursos/IMG/Astrocamp_Square.png" class="img-fluid rounded-start" style="border-radius: 0.5rem;" alt="...">
+          </div>
+          <div class="col-xl-8 col-lg-7  p-4">
+            <div class="card-body" style="background-color:#81a05a; border: 8px solid #198754; border-radius: 0.5rem;">
+              <h3 class="card-title">Astrocampo</h3>
+              <p class="card-text">O AstroCamp é um projeto académico voltado para o estudo da astronomia e física, que oferece
+                aos participantes uma formação de alta qualidade e inspiradora, além de um ambiente tranquilo
+                e isolado. O programa é direcionado a estudantes do ensino secundário, que serão selecionados com base na sua
+                motivação e desempenho académico. A participação para alunos portugueses no programa tem
+                zero custos adicionais.</p>
+              <a style="background-color: #3c6255; border: 0;" href="https://astrocamp.astro.up.pt/pt/programa.html" target="_blank" class="btn btn-primary">Ver mais...</a>
+            </div>
+          </div>
+        </div>
       </div>
 
+      <div class="card mb-3 align-items-center ">
+        <div class="row d-flex align-items-center w-100">
+          <div class="col-xl-4 col-lg-5 col  p-4">
+            <img src="../recursos/IMG/ceia/CEIA.png" class="img-fluid rounded-start" style="border-radius: 0.5rem;" alt="...">
+          </div>
+          <div class="col-xl-8 col-lg-7  p-4">
+            <div class="card-body" style="background-color:#81a05a; border: 8px solid #198754; border-radius: 0.5rem;">
+              <h3 class="card-title">Astrocampo</h3>
+              <p class="card-text">O AstroCamp é um projeto académico voltado para o estudo da astronomia e física, que oferece
+                aos participantes uma formação de alta qualidade e inspiradora, além de um ambiente tranquilo
+                e isolado. O programa é direcionado a estudantes do ensino secundário, que serão selecionados com base na sua
+                motivação e desempenho académico. A participação para alunos portugueses no programa tem
+                zero custos adicionais.</p>
+              <a style="background-color: #3c6255; border: 0;" href="https://astrocamp.astro.up.pt/pt/programa.html" target="_blank" class="btn btn-primary">Ver mais...</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3 align-items-center ">
+        <div class="row d-flex align-items-center w-100">
+          <div class="col-xl-4 col-lg-5 col  p-4">
+            <img src="../recursos/IMG/Astrocamp_Square.png" class="img-fluid rounded-start" style="border-radius: 0.5rem;" alt="...">
+          </div>
+          <div class="col-xl-8 col-lg-7  p-4">
+            <div class="card-body" style="background-color:#81a05a; border: 8px solid #198754; border-radius: 0.5rem;">
+              <h3 class="card-title">Astrocampo</h3>
+              <p class="card-text">O AstroCamp é um projeto académico voltado para o estudo da astronomia e física, que oferece
+                aos participantes uma formação de alta qualidade e inspiradora, além de um ambiente tranquilo
+                e isolado. O programa é direcionado a estudantes do ensino secundário, que serão selecionados com base na sua
+                motivação e desempenho académico. A participação para alunos portugueses no programa tem
+                zero custos adicionais.</p>
+              <a style="background-color: #3c6255; border: 0;" href="https://astrocamp.astro.up.pt/pt/programa.html" target="_blank" class="btn btn-primary">Ver mais...</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Boxes end here -->
     </div>
-  </div>
-
-  <!-- Boxes end here -->
-  </div>
 
 
-  <?php
+    <?php
 
-  include_once __DIR__ . '/Compunents/footer.php';
+    include_once __DIR__ . '/Compunents/footer.php';
 
 
-  ?>
+    ?>
