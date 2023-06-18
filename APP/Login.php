0665096c94704e2b9190eaec4eff730f07910ebe
henriqueolivgp <henrique.o@ipvc.pt>
2023-06-18 22:48:16 +0100

    login.php

--- APP/Login.php
+++ APP/Login.php
@@ -27,7 +27,7 @@ include_once __DIR__ . '/Compunents/header.php';
     </div>
   </nav>
 
-  <div class="container vh-100 d-flex align-items-center justify-content-center text-center bg-light">
+  <div class="d-flex align-items-center justify-content-center text-center bg-light">
     <div class="w-75">
       <main>
         <section>
@@ -63,6 +63,48 @@ include_once __DIR__ . '/Compunents/header.php';
     </div>
   </div>
 
-  <?php
-  include_once __DIR__ . '/Compunents/footer.php';
-  ?>
+  <!-- Site footer -->
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h6>About</h6>
+          <p class="text-justify">Website realizado com base na matéria aprendida nas aulas de Programação Web. Se desejar ver o nosso Repositório do GitHub ou o nosso perfil do GitHub basta carregar em Repositório ou no nosso nome</p>
+          <p>Trabalho lecionado pelos professores: António José Viana, Marcelo Antunes Fernandes e Wenderson Wanzeller</p>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>Quick Link</h6>
+          <ul class="footer-links">
+            <li><a style="text-decoration: none;" href="https://github.com/henriqueolivgp/Trabalho_PW_Parte_2" target="_blank">Repositório</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2023 Este trabalho foi realizado por
+            <a style="text-decoration: none;" href="https://github.com/henriqueolivgp" target="_blank">Henrique Oliveira</a> e <a style="text-decoration: none;" href="https://github.com/pedrof04" target="_blank">Pedro Ferreira</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script>
+
+    var loader = document.getElementById("preloader");
+
+    window.addEventListener("load", function(){
+      loader.style.display = "none";
+    })
+
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+  </body>
+
+  </html>
