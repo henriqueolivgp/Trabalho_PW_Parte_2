493fa41f1d3e116ca67017b0a8bd98d5355dde0c
henriqueolivgp <henrique.o@ipvc.pt>
2023-06-18 16:59:07 +0100

    Prealoader_Working

--- APP/Compunents/footer.php
+++ APP/Compunents/footer.php
@@ -27,8 +27,18 @@
     </div>
   </footer>
 
+  <script>
+
+    var loader = document.getElementById("preloader");
+
+    window.addEventListener("load", function(){
+      loader.style.display = "none";
+    })
+
+  </script>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
-</body>
+  </body>
 
-</html>
+  </html>
