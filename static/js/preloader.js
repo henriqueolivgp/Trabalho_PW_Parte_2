8548a02d50991ad2c9ca069abc9f4a21769516e1
henriqueolivgp <henrique.o@ipvc.pt>
2023-06-18 21:49:44 +0100

    Some changes in preloader

--- static/js/preloader.js
+++ static/js/preloader.js
@@ -1,11 +1,5 @@
-$(document).ready(function() {
-    setTimeout(function() {
-      $('#container').addClass('loaded');
-      // Once the container has finished, the scroll appears
-      if ($('#container').hasClass('loaded')) {
-        // It is so that once the container is gone, the entire preloader section is deleted
-        $('#preloader').delay(9000).queue(function() {
-          $(this).remove();
-        });}
-    }, 2000);});
-  
+var loader = document.getElementById("preloader");
+
+    window.addEventListener("load", function(){
+      loader.style.display = "none";
+    })
