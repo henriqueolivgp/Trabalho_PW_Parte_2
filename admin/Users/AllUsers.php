1b848952aa65e3fc5eaa9b75e144de8ed1e3a499
henriqueolivgp <henrique.o@ipvc.pt>
2023-06-20 15:48:39 +0100

    Back-end

--- admin/Users/AllUsers.php
+++ admin/Users/AllUsers.php
@@ -16,10 +16,11 @@ require_once __DIR__ . '/../../APP/Compunents/header.php';
 
     ?>
 
-    <h1 class="text-center">Gerenciar Usuários</h1>
+    <h1 class="text-center p-2">Gerenciar Usuários</h1>
 
-    <div class="container d-flex flex-column">
+    <div class="container d-flex flex-column bg-light">
         <div class="content">
+            <a href="/admin/Admin.php"><button type="button" class="btn btn-secondary px-5 me-2 mb-2 mt-2">Voltar</button></a>
             <div class="table-responsive">
                 <table class="table table-striped table-bordered">
                     <thead>
@@ -27,13 +28,11 @@ require_once __DIR__ . '/../../APP/Compunents/header.php';
                             <th scope="col">Id</th>
                             <th scope="col">Nome</th>
                             <th scope="col">Apelido</th>
-                            <th scope="col">NIF</th>
                             <th scope="col">Telemovel</th>
                             <th scope="col">Email</th>
                             <th scope="col">Foto</th>
                             <th scope="col">Adm.</th>
                             <th scope="col">Dono</th>
-                            <th scope="col">PW</th>
                             <th scope="col">Ações</th>
                         </tr>
                     </thead>
@@ -49,16 +48,14 @@ require_once __DIR__ . '/../../APP/Compunents/header.php';
                                 <th><?php echo $utilizador['id']; ?></th>
                                 <td><?php echo $utilizador['nome']; ?></td>
                                 <td><?php echo $utilizador['apelido']; ?></td>
-                                <td><?php echo $utilizador['nif']; ?></td>
                                 <td><?php echo $utilizador['telemovel']; ?></td>
                                 <td><?php echo $utilizador['email']; ?></td>
                                 <td><?php echo $utilizador['foto']; ?></td>
-                                <td class="w-auto"><?php echo $utilizador['administrador']; ?></td>
+                                <td><?php echo $utilizador['administrador']; ?></td>
                                 <td><?php echo $utilizador['dono']; ?></td>
-                                <td class="text-truncate" style="max-width: 200px;"><?php echo $utilizador['palavra_passe']; ?></td>
                                 <td>
-                                    <button class="btn btn-info" href="editar.php?id=<?php echo $utilizador['id']; ?>">Editar</button>
-                                    <button class="btn btn-danger" href="excluir.php?id=<?php echo $utilizador['id']; ?>">Excluir</button>
+                                    <a href="../../src/controllers/admin/controlar-utilizador.php?<?= 'utilizador=atualizar&id=' . $utilizador['id'] ?>"><button type="button" class="btn btn-info">Editar</button></a>
+                                    <a href="../../src/controllers/admin/controlar-utilizador.php?<?= 'utilizador=deletar&id=' . $utilizador['id'] ?>"><button class="btn btn-danger" type="submit">Excluir</button></a>
                                 </td>
                             </tr>
                         <?php
@@ -67,8 +64,8 @@ require_once __DIR__ . '/../../APP/Compunents/header.php';
                     </tbody>
                 </table>
             </div>
-        </div>
 
+        </div>
     </div>
 
     <?php
--- static/js/scroll.js
+++
@@ -1,9 +0,0 @@
-function disableScrolling(){
-    var x=window.scrollX;
-    var y=window.scrollY;
-    window.onscroll=function(){window.scrollTo(x, y);};
-}
-
-function enableScrolling(){
-    window.onscroll=function(){};
-}
